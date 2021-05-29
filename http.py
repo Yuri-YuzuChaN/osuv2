e9f5e58aee1f7dd83f2ae830799a6cca4387364b
Yuri-YuzuChaN <65105396+Yuri-YuzuChaN@users.noreply.github.com>
2021-05-29 15:21:50 +0800

    HTTP服务访问本地音乐和图片文件

---
+++ http.py
@@ -0,0 +1,17 @@
+import nonebot, os, filetype
+from quart import request, make_response
+
+PATH = os.path.join(os.path.dirname(__file__), 'osufile', 'map')
+
+app = nonebot.get_bot().server_app
+
+@app.route('/map/<int:mapid>/<string:filename>')
+async def music(mapid, filename):
+    if request.method == 'GET':
+        if filename and '.osu' not in filename:
+            file = os.path.join(PATH, str(mapid), filename)
+            content_type = filetype.guess(file).mime
+            image = open(file, 'rb').read()
+            response = await make_response(image)
+            response.headers['Content-Type'] = content_type
+            return response
