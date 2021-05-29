f1c95cbbda0fe8774f19655d1dbab440197b1af8
Yuri-YuzuChaN <65105396+Yuri-YuzuChaN@users.noreply.github.com>
2021-05-29 15:23:06 +0800

    移除oppai模块

--- pp.py
+++ pp.py
@@ -1,4 +1,3 @@
-from .oppai.oppai import *
 from maniera.calculator import Maniera
 import pyttanko as osu
 
@@ -27,25 +26,18 @@ def calc_pp(osufile, mods_num, maxcb, c50, c100, c300, miss):
         bmap = p.map(f)
 
     stars = osu.diff_calc().calc(bmap, mods_num)
-    map_stars = stars.total
 
     pp, aim, speed, acc, accuracy = osu.ppv2(
         stars.aim, stars.speed, mods=mods_num,
         combo=maxcb, n300=c300, n100=c100, n50=c50, nmiss=miss, bmap=bmap
     )
 
-    # cs = bmap.cs
-    # ar = bmap.ar
-    # od = bmap.od
-    # hp = bmap.hp
-    play_pp = int(round(pp, 2))
-    aim_pp = int(round(aim, 2))
-    speed_pp = int(round(speed, 2))
-    acc_pp = int(round(acc, 2))
-    accuracy = round(accuracy, 2)
+    play_pp = int(pp)
+    aim_pp = int(aim)
+    speed_pp = int(speed)
+    acc_pp = int(acc)
     return play_pp, aim_pp, speed_pp, acc_pp
-    # return map_stars, cs, ar, od, hp, play_pp, aim_pp, speed_pp, acc_pp, accuracy
-    
+   
 def calc_if(osufile, mods_num, c50, c100, mapcb):
     p = osu.parser()
     with open(osufile, 'r', encoding='utf-8') as f:
@@ -64,14 +56,6 @@ def calc_mania_pp(osufile, mods_num, score):
     calc = Maniera(osufile, mods_num, score)
     calc.calculate()
     return int(calc.pp)
-
-def calc_time(osufile, c50, c100, c300, miss):
-    setcb = c50 + c100 + c300 + miss
-    ez = ezpp_new()
-    ezpp_dup(ez, osufile)
-    play_time = ezpp_time_at(ez, setcb)
-    ezpp_free(ez)
-    return play_time
 
 def calc_acc(mode, c50, c100, c300, cmiss, ckatu, cgeki):
     if mode == 0:
